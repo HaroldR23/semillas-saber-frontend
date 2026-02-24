cd3dd93ae785263c5659373607c6ccd0ce9901b1
HaroldR23 <ahsro99@gmail.com>
2026-02-24 19:49:52 -0300

    feat: implement Header and Footer layout components

---
+++ app/constants/index.ts
@@ -0,0 +1,7 @@
+export const menuItems = [
+  { label: 'Materias', id: 'materias' },
+  { label: 'Servicios', id: 'servicios' },
+  { label: 'Precios', id: 'precios' },
+  { label: 'Contacto', id: 'contacto' },
+];
+export const waMessage = "https://wa.me/573124184842?text=Hola%20Semillas%20del%20Saber%2C%20necesito%20ayuda%20con%20mis%20tareas.";
