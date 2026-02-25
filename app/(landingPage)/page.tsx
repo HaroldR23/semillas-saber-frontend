78f165a5f5f42effff5a02beded22c2fa8ad6872
HaroldR23 <ahsro99@gmail.com>
2026-02-25 08:37:10 -0300

    feat: implement pricing component section

--- app/(landingPage)/page.tsx
+++ app/(landingPage)/page.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Benefits from "./components/Benefits";
 import Hero from "./components/Hero";
 import HowItWorks from "./components/HowItWorks";
+import Pricing from "./components/Pricing";
 import Services from "./components/Services";
 import SubjectsSection from "./components/SubjectsSection/SubjectsSection";
 
@@ -12,6 +15,7 @@ const Home = () => {
       <HowItWorks />
       <Services />
       <Benefits />
+      <Pricing onSelectPlan={(plan) => console.log(`Selected plan: ${plan}`)} />
     </div>
   );
 };
